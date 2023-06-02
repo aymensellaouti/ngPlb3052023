314a3141717406ec96b0848aa171d2451e944228
aymen2782 <aymen.sellaouti@gmail.com>
2023-06-02 09:58:46 +0100

    fin router et correction exercice details et suppression cv

--- src/app/cv/cv-infos/cv-infos.component.ts
+++ src/app/cv/cv-infos/cv-infos.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CvService } from '../services/cv.service';
+import { Cv } from '../model/cv.model';
+import { MES_ROUTES } from 'src/app/config/routes.config';
 
 @Component({
   selector: 'app-cv-infos',
   templateUrl: './cv-infos.component.html',
-  styleUrls: ['./cv-infos.component.css']
+  styleUrls: ['./cv-infos.component.css'],
 })
 export class CvInfosComponent {
-  constructor(private activatedRoute: ActivatedRoute) {
+  cv: Cv | null = null;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private cvService: CvService,
+    private router: Router
+  ) {
     console.log(activatedRoute.snapshot);
+    // Je récupére mon id
+    const id = this.activatedRoute.snapshot.params['id'];
+    // Je chercher le cv de cet id la
+    // Si le cv existe je l'affiche
+    this.cv = this.cvService.getCvById(+id);
+    // Sinon je redirige vers la page de la liste des cvs
+    if (!this.cv) {
+      this.router.navigate([MES_ROUTES.cv]);
+    }
   }
-
+  onDelete() {
+    if(this.cv) {
+      this.cvService.deleteCv(this.cv);
+      this.router.navigate([MES_ROUTES.cv]);
+    }
+  }
 }
--- src/app/cv/services/cv.service.ts
+++ src/app/cv/services/cv.service.ts
@@ -33,4 +33,15 @@ export class CvService {
   getCvs(): Cv[] {
     return this.cvs;
   }
+  getCvById(id: number): Cv | null {
+    return this.cvs.find((cv) => cv.id === id ) ?? null;
+  }
+  deleteCv(cv: Cv): boolean {
+    const index = this.cvs.indexOf(cv);
+    if (index != -1) {
+      this.cvs.splice(index, 1);
+      return true;
+    }
+    return false;
+  }
 }
